38c50e19abfd410973fe2c375c0a4817fc815257
5c5 <colas.p.s@gmail.com>
2014-12-23 10:00:06 +0100

    maj moteur

--- BasesAI/TP/moteur.pl
+++ BasesAI/TP/moteur.pl
@@ -9,7 +9,7 @@ si([T|R]):-vrai(T), si(R).
 alors([X]):- assert(vrai(X)).
 alors([T|R]):-assert(vrai(T)), alors(R).
 
-# Déclaration des règles
+%# Déclaration des règles
 regle(r1):- si([a, b]), alors([c]).
 regle(r2):- si([c, d]), alors([f]).
 regle(r3):- si([f, b]), alors([e]).
@@ -17,26 +17,28 @@ regle(r4):- si([f, a]), alors([g]).
 regle(r5):- si([g, f]), alors([b]).
 regle(r6):- si([a, h]), alors([l]).
 
-# prédicat pour l'ajout des faits, ce qui est vrai et ce qui est faux
+%# prédicat pour l'ajout des faits, ce qui est vrai et ce qui est faux
 faits([X]):-assert(vrai(X)).
 faits([T|R]):-assert(vrai(T)), faits(R).
 
 mefaits([X]):-assert(faux(X)).
 mefaits([T|R]):-assert(faux(T)), mefaits(R).
 
-# Prédicat d'effaçage des faits de prolog
+%# Prédicat d'effaçage des faits de prolog
 raz:-retractall(vrai(_)), retractall(faux(_)), retractall(marquer(_)).
 
-# Affichage des faits vrai(?)
-lister([]):-vrai(X), lister([X]).
-lister(L):-vrai(X), not(member(X, L)), lister([X|L]).
-lire([X]):-write(X).
-lister([T|H]):- write(T), lire(H).
-lire([T|H]):-write(T), lire(H).
+%# Affichage des faits vrai(?)
+lire([X]):-write(X), nl, !.
+lire([T|H]):-write(T), write(' '), lire(H).
 
-# Application des changement
-appliquer:- changement, retract(changement), not(marquer(X)), regle(X), assert(marquer(X)),write(X), write( : ),lister([]) ,assert(changement), fail.
+lister([]):-vrai(X), lister([X]).
+lister(L):-  vrai(X), not(member(X, L)), lister([X|L]).
+lister([T|H]):- write(T), write(' '), lire(H).
+
+%# Application des changement
+application:- not(marquer(X)), regle(X), assert(marquer(X)),write(X), write(' : '),lister([]).
+appliquer:- changement, retract(changement), !, application, assert(changement), fail.
 appliquer:- not(changement).
 
-# Lancement du chainage avant
-saturer:- assert(changement), appliquer.
+%# Lancement du chainage avant
+saturer:- assert(changement), !, appliquer.
